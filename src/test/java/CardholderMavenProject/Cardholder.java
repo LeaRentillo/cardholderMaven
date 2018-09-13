49a6ea1b0118212d280f2de20e5c2bc3110cd18a
Dell <Dell@192.168.3.87>
2018-09-13 09:24:01 +0800

    testing order

--- src/test/java/CardholderMavenProject/Cardholder.java
+++ src/test/java/CardholderMavenProject/Cardholder.java
@@ -1413,7 +1413,7 @@ public void OrderCard1(){
 		WebElement parseIndex = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-content\"]/div[1]/div/h1")));
 		System.out.println(parseIndex.getText());
 		
-		driver.get("https://dev.cardholder.an-other.co.uk/");
+		//driver.get("https://dev.cardholder.an-other.co.uk/");
 		WebElement orderButton = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class='btn btn-default btn-activate-new-card']"))); 
 		orderButton.click();
 		wait = new WebDriverWait(driver, 20);
