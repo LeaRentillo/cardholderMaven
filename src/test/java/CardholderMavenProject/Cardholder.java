d85084eb7f66f3678c53129cd5200e64303ea528
Dell <Dell@192.168.3.87>
2018-09-13 09:17:53 +0800

    testing order

--- src/test/java/CardholderMavenProject/Cardholder.java
+++ src/test/java/CardholderMavenProject/Cardholder.java
@@ -1385,12 +1385,23 @@ public void OrderCard1(){
 	settings testSettings = new settings();
 	if(testSettings.skipTest("orderCardTest1")){
 		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Documents\\LEA\\SELENIUM\\chromedriver_win32\\chromedriver.exe");
-		////driver = new ChromeDriver();
-		//**********************************//	   
+		driver = new ChromeDriver();
+		//**********************************//	 
 		
-		/*wait = new WebDriverWait(driver, 20);   
-		driver.manage().window().maximize();
-		driver.get("https://dev.cardholder.an-other.co.uk/");
+		  wait = new WebDriverWait(driver, 20);   
+		   
+		   driver.manage().window().maximize();
+		   driver.get("https://dev.cardholder.an-other.co.uk/");
+		   
+			WebElement LoginLogo = driver.findElement (By.cssSelector("img[class='login-logo']"));
+			Assert.assertTrue(LoginLogo.isDisplayed());
+			WebElement forgotPassword = driver.findElement (By.xpath("//a[@href='/user/recovery/request']"));
+			forgotPassword.click();
+			
+			//Navigate Back
+			driver.navigate().back();
+		
+		
 
 		WebElement SignIn1 = driver.findElement(By.xpath("//*[@id=\"LoginForm\"]/button"));
 		WebElement Username1 = driver.findElement(By.xpath("//*[@id=\"loginform-login\"]"));
@@ -1401,7 +1412,7 @@ public void OrderCard1(){
 		
 		WebElement parseIndex = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-content\"]/div[1]/div/h1")));
 		System.out.println(parseIndex.getText());
-		*/
+		
 		driver.get("https://dev.cardholder.an-other.co.uk/");
 		WebElement orderButton = new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class='btn btn-default btn-activate-new-card']"))); 
 		orderButton.click();
